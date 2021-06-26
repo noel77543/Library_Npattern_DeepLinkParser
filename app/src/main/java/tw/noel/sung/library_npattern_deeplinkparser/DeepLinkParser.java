d2b7709d63daaea894ef13f4d9335bd66f627e00
noel77543 <noel77543@gmail.com>
2021-06-26 14:41:19 +0800

    調整調用方式

--- app/src/main/java/tw/noel/sung/library_npattern_deeplinkparser/DeepLinkParser.java
+++ app/src/main/java/tw/noel/sung/library_npattern_deeplinkparser/DeepLinkParser.java
@@ -7,31 +7,37 @@ import java.lang.reflect.Field;
 
 import tw.noel.sung.library_npattern_deeplinkparser.annotation.NParameter;
 
-public class DeepLinkParser<T> {
+public class DeepLinkParser {
 
-    public T parse(String linkUrl, T parameterData) throws IllegalAccessException {
-        Uri uri = Uri.parse(linkUrl);
-        //取得所有宣告的變數
-        Field[] fields = parameterData.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            //取其變數名稱
-            String queryName= field.getName();
+    public <T> T parse(String linkUrl, Class<T> parameterDataClass) {
+        T parameterObject = null;
+        try {
+            parameterObject = (T) Class.forName(parameterDataClass.getName()).newInstance();
 
-            //如果有下NParameter 的annotation 則以此annotation的值為準
-            if(field.isAnnotationPresent(NParameter.class)){
-                NParameter nParameter = field.getAnnotation(NParameter.class);
-                if(nParameter!=null){
-                    queryName = nParameter.value();
+            Uri uri = Uri.parse(linkUrl);
+            //取得所有宣告的變數
+            Field[] fields = parameterDataClass.getDeclaredFields();
+            for (Field field : fields) {
+                //取其變數名稱
+                String queryName = field.getName();
+
+                //如果有下NParameter 的annotation 則以此annotation的值為準
+                if (field.isAnnotationPresent(NParameter.class)) {
+                    NParameter nParameter = field.getAnnotation(NParameter.class);
+                    if (nParameter != null) {
+                        queryName = nParameter.value();
+                    }
+                }
+
+                String parameterValue = uri.getQueryParameter(queryName);
+                if (parameterValue != null) {
+                    field.setAccessible(true);
+                    field.set(parameterObject, parameterValue);
                 }
             }
-
-            String parameterValue = uri.getQueryParameter(queryName);
-            if (parameterValue != null) {
-                field.setAccessible(true);
-                field.set(parameterData, parameterValue);
-            }
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        return parameterData;
+        return parameterObject;
     }
-
 }
